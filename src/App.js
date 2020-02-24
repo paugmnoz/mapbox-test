7f5930f795a04a92e4647fba4f0c0c71b2f9cbfc
Paula <paugmnoz@gmail.com>
2020-02-24 16:59:28 -0500

    Map set up with pop up on click

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import MapPage from './containers/MapPage';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        // <MapPage />
+      }
+      <div className='maptool'>
+        <span className='weather-icon'>
+          <img src='https://image.flaticon.com/icons/svg/2121/2121548.svg' alt=''/>
+        </span>
+      </div>
     </div>
   );
 }
