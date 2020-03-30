47695dcd52bc8252cb27bdd82834021b1fd0c792
Paula <paula.garcia1@correo.icesi.edu.co>
2020-03-30 12:12:28 -0500

    Updates

---
+++ precache-manifest.b7ae33557440c0f42e9793324767d944.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b3994f7ad5043ffa03b2d141e0134e7",
+    "url": "/mapbox-test/index.html"
+  },
+  {
+    "revision": "2ec58d2953b72f8721eb",
+    "url": "/mapbox-test/static/css/main.e7dd2de8.chunk.css"
+  },
+  {
+    "revision": "5dae29e8fa20cdb77515",
+    "url": "/mapbox-test/static/js/2.eb4208a2.chunk.js"
+  },
+  {
+    "revision": "0c358c24b2cb9d01c5fadeff0f12ef98",
+    "url": "/mapbox-test/static/js/2.eb4208a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ec58d2953b72f8721eb",
+    "url": "/mapbox-test/static/js/main.5a4c7f56.chunk.js"
+  },
+  {
+    "revision": "8e4f4c59990616665139",
+    "url": "/mapbox-test/static/js/runtime-main.908e5775.js"
+  },
+  {
+    "revision": "cf77e17c135eed3c067973836c9222c1",
+    "url": "/mapbox-test/static/media/dot.cf77e17c.svg"
+  }
+]);
